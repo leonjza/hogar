1d7d4162bf0f0049c7f12886ed107c3b801e0dc0
Leon Jacobs <leonja511@gmail.com>
2015-06-29 17:41:24 +0200

    Add the whoami plugin

---
+++ hogar/Plugins/Whoami/main.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Leon Jacobs
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+''' Returns technical information about the sender '''
+
+def applicable_types():
+
+    '''
+        Applicable Types
+
+        Returns the type of messages this plugin is for.
+        See: hogar.static.values
+
+        --
+        @return list
+    '''
+
+    return ['text']
+
+def commands():
+
+    '''
+        Commands
+
+        In the case of text plugins, returns the commands
+        that this plugin should trigger for. For other
+        message types, a empty list should be returned.
+
+        --
+        @return list
+    '''
+
+    return ['whoami']
+
+def should_reply():
+
+    '''
+        Should Reply
+
+        Specifies wether a reply should be sent to the original
+        sender of the message that triggered this plugin.
+
+        --
+        @return bool
+    '''
+
+    return True
+
+def reply_type():
+
+    '''
+        Reply Type
+
+        Specifies the type of reply that should be sent to the
+        sender. This is an optional function. See hogar.static.values
+        for available types.
+
+        --
+        @return str
+    '''
+
+    return 'text'
+
+def run(message):
+
+    '''
+        Run
+
+        Run the custom plugin specific code. A returned
+        string is the message that will be sent back
+        to the user.
+
+        --
+        @param  message:dict    The message sent by the user
+
+        @return str
+    '''
+
+    return 'Your Telegram ID is: {id}'.format(id = message['from']['id'])
